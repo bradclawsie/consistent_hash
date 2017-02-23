7de1b12944bd2531deba622ca03ef54f95cb1d3d
brad clawsie <brad@b7j0c.org>
2017-02-22 20:28:34 -0800

    golint, go vet

--- consistent_hash.go
+++ consistent_hash.go
@@ -1,63 +1,59 @@
-// A consistent hash library
-package consistent_hash 
+// Package consistenthash implements a consistent hash.
+package consistenthash
 
 import (
-	"fmt"
-	"log"
 	"errors"
+	"fmt"
 	"hash/crc32"
-	"sync"
 )
 
+// ConsistentHash maps hashed values to targets.
 type ConsistentHash struct {
 	// to enter elements into the hash multiple times
 	Mult int
 	// the sorted list of hashed elements
 	SumList []uint32
 	// a map of hash values to original strings
-	Source (map [uint32] string)
-	// lock for access
-	mutex sync.RWMutex
+	Source (map[uint32]string)
 }
 
 // NewConsistentHash will create a new consistent hash with hashed elements multiplied and entered
 // into the SumList "mult" times.
-func NewConsistentHash(mult int) (*ConsistentHash) {
+func NewConsistentHash(mult int) (*ConsistentHash, error) {
 	if mult <= 0 {
-		log.Print("mult factor must be 1 or greater")
-		return nil
+		return nil, errors.New("mult factor must be 1 or greater")
 	}
 	h := new(ConsistentHash)
-	h.SumList = make([]uint32,0)
+	h.SumList = make([]uint32, 0)
 	h.Mult = mult
-	h.Source = make(map [uint32] string)
-	return h
+	h.Source = make(map[uint32]string)
+	return h, nil
 }
 
 // New is an alias to NewConsistentHash.
-func New(mult int) (*ConsistentHash) {
+func New(mult int) (*ConsistentHash, error) {
 	return NewConsistentHash(mult)
 }
 
-// the way we "multiply" a string...we simply append and integer to the end
-func mult_elt(s string,i int) string {
-	return fmt.Sprintf("%s.%d",s,i)
+// multElt is the way we "multiply" a string...we simply append and integer to the end.
+func multElt(s string, i int) string {
+	return fmt.Sprintf("%s.%d", s, i)
 }
 
-// insert a new element into the SumList. this list is kept sorted.
-func (h *ConsistentHash) insert_one(s,s_mult string) error {
-	s_sum := crc32.ChecksumIEEE([]byte(s_mult))
-	hl := make([]uint32,len(h.SumList)+1)
+// insertOne inserts a new element into the SumList. this list is kept sorted.
+func (h *ConsistentHash) insertOne(s, sMult string) error {
+	sSum := crc32.ChecksumIEEE([]byte(sMult))
+	hl := make([]uint32, len(h.SumList)+1)
 	i := 0
 	done := false
-	for _,v := range h.SumList {
-		if s_sum == v {
+	for _, v := range h.SumList {
+		if sSum == v {
 			// duplicate entry
-			e := fmt.Sprintf("collision on %s hashed as %d",s,s_sum)
+			e := fmt.Sprintf("collision on %s hashed as %d", s, sSum)
 			return errors.New(e)
 		}
-		if s_sum <= v && !done {
-			hl[i] = s_sum
+		if sSum <= v && !done {
+			hl[i] = sSum
 			i++
 			done = true
 		}
@@ -65,87 +61,77 @@ func (h *ConsistentHash) insert_one(s,s_mult string) error {
 		i++
 	}
 	if !done {
-		hl[i] = s_sum
+		hl[i] = sSum
 	}
-	h.SumList = hl	
-	h.Source[s_sum] = s
+	h.SumList = hl
+	h.Source[sSum] = s
 	return nil
 }
 
 // Insert a new element into the SumList as "mult" instances of crc32 hashes.
 func (h *ConsistentHash) Insert(s string) error {
-	h.mutex.Lock()
-	defer h.mutex.Unlock()
 	for i := 1; i <= h.Mult; i++ {
-		insert_err := h.insert_one(s,mult_elt(s,i))
-		if insert_err != nil {
-			return insert_err
+		insertErr := h.insertOne(s, multElt(s, i))
+		if insertErr != nil {
+			return insertErr
 		}
-	} 
+	}
 	return nil
 }
 
 // remove a single element from SumList
-func (h *ConsistentHash) remove_one(s_mult string) error {
-	s_sum := crc32.ChecksumIEEE([]byte(s_mult))
-	if _,source_ok := h.Source[s_sum]; !source_ok {
-		log.Printf("%s not found",s_mult)
-		return nil
+func (h *ConsistentHash) removeOne(sMult string) error {
+	sSum := crc32.ChecksumIEEE([]byte(sMult))
+	if _, ok := h.Source[sSum]; !ok {
+		return nil // not found
 	}
-	hl := make([]uint32,len(h.SumList)-1)
+	hl := make([]uint32, len(h.SumList)-1)
 	i := 0
-	for _,v := range h.SumList {
-		if v != s_sum {
+	for _, v := range h.SumList {
+		if v != sSum {
 			hl[i] = v
 			i++
 		}
 	}
-	h.SumList = hl	
-	delete(h.Source,s_sum)
+	h.SumList = hl
+	delete(h.Source, sSum)
 	return nil
 }
 
 // Remove a new element from the SumList as "mult" instances of crc32 hashes.
 func (h *ConsistentHash) Remove(s string) error {
-	h.mutex.Lock()
-	defer h.mutex.Unlock()
 	for i := 1; i <= h.Mult; i++ {
-		remove_err := h.remove_one(mult_elt(s,i))
-		if remove_err != nil {
-			return remove_err
+		removeErr := h.removeOne(multElt(s, i))
+		if removeErr != nil {
+			return removeErr
 		}
-	} 
+	}
 	return nil
 }
 
-// Find the nearest hashed element (in ascending order) 
+// Find the nearest hashed element (in ascending order)
 // that the candidate string s maps to. should s hash to a greater
 // value than the maximum hashed item in the SumList, loop around
 // and select the zeroth hashed element
-func (h *ConsistentHash) Find(s string) (string,error) {
-	h.mutex.RLock()
-	defer h.mutex.RUnlock()	
+func (h *ConsistentHash) Find(s string) (string, error) {
 	if len(h.SumList) == 0 {
 		e := fmt.Sprintf("empty sumlist")
-		return "",errors.New(e)		
+		return "", errors.New(e)
 	}
-	s_sum := crc32.ChecksumIEEE([]byte(s))
-	for _,v := range h.SumList {
-		if s_sum <= v {
-			if src,src_ok := h.Source[v]; src_ok {
-				return src,nil
-			} else {
-				e := fmt.Sprintf("no source mapping for %v",v)
-				return "",errors.New(e)
+	sSum := crc32.ChecksumIEEE([]byte(s))
+	for _, v := range h.SumList {
+		if sSum <= v {
+			if src, ok := h.Source[v]; ok {
+				return src, nil
 			}
+			e := fmt.Sprintf("no source mapping for %v", v)
+			return "", errors.New(e)
 		}
 	}
 	v := h.SumList[0] // if not valid, first line would have triggered
-	if src,src_ok := h.Source[v]; src_ok {
-		return src,nil
-	} else {
-                e := fmt.Sprintf("no source mapping for %v",v)
-                return "",errors.New(e)
-        }
+	if src, ok := h.Source[v]; ok {
+		return src, nil
+	}
+	e := fmt.Sprintf("no source mapping for %v", v)
+	return "", errors.New(e)
 }
-
--- consistent_hash_test.go
+++ consistent_hash_test.go
@@ -1,87 +1,87 @@
-package consistent_hash 
+package consistenthash
 
 import (
-	"testing"
+	"encoding/base64"
 	"fmt"
-	"sort"
-	"strconv"
 	"hash/crc32"
 	"math/rand"
-	"encoding/base64"
+	"sort"
+	"strconv"
+	"testing"
 )
 
 func TestBuildCH(t *testing.T) {
 	fmt.Printf("\n---\nTEST BUILD, INSERT AND FIND\n")
 	mult := 100
-	ch := NewConsistentHash(mult)
-	items := []string{"127.0.0.1","17.0.1.1","1.1.0.1","27.99.0.111","64.0.8.8","8.8.8.8","10.100.0.100",
-		"128.4.4.4","28.28.1.1","28.10.0.10","12.9.0.10","11.11.8.1","13.10.0.19","128.19.19.19"}
-	for _,item := range items {
-		insert_err := ch.Insert(item)
-		if insert_err != nil {
-			t.Errorf(insert_err.Error())
+	ch, _ := NewConsistentHash(mult)
+	items := []string{"127.0.0.1", "17.0.1.1", "1.1.0.1", "27.99.0.111", "64.0.8.8", "8.8.8.8", "10.100.0.100",
+		"128.4.4.4", "28.28.1.1", "28.10.0.10", "12.9.0.10", "11.11.8.1", "13.10.0.19", "128.19.19.19"}
+	for _, item := range items {
+		insertErr := ch.Insert(item)
+		if insertErr != nil {
+			t.Errorf(insertErr.Error())
 		}
 	}
 	// make sure the len is correct
 	if len(ch.SumList) != (mult * len(items)) {
-		e := fmt.Sprintf("SumList len should be %d, but is %d",(mult*len(items)),len(ch.SumList))
+		e := fmt.Sprintf("SumList len should be %d, but is %d", (mult * len(items)), len(ch.SumList))
 		t.Errorf(e)
 	}
 	// make sure the list is indeed sorted
-	sl := make([]int,len(ch.SumList))
-	for k,v := range ch.SumList {
+	sl := make([]int, len(ch.SumList))
+	for k, v := range ch.SumList {
 		sl[k] = int(v)
 	}
 	sort.Ints(sl)
-	for k,v := range sl {
+	for k, v := range sl {
 		if v != int(ch.SumList[k]) {
-			e := fmt.Sprintf("sorted list differs at position %d",k)
+			e := fmt.Sprintf("sorted list differs at position %d", k)
 			t.Errorf(e)
 		}
 	}
-	count := make(map [string] int)
-	for _,sum := range ch.SumList {
+	count := make(map[string]int)
+	for _, sum := range ch.SumList {
 		count[ch.Source[sum]]++
 	}
-	for k,c := range count {
+	for k, c := range count {
 		if c != mult {
-			e := fmt.Sprintf("%s appears %d times, not %d times",k,c,mult)
+			e := fmt.Sprintf("%s appears %d times, not %d times", k, c, mult)
 			t.Errorf(e)
 		}
 	}
-	nearest_hash,nh_err := ch.Find("hello")
-	if nh_err != nil || nearest_hash == "" {
+	nearestHash, nhErr := ch.Find("hello")
+	if nhErr != nil || nearestHash == "" {
 		t.Errorf("err returned on finding nearest hash for hello")
 	}
 
 	// test distribution
-	dist := make(map [string] int)
+	dist := make(map[string]int)
 	total := 10000
 	for j := 0; j < total; j++ {
 		b64 := base64.StdEncoding.EncodeToString([]byte(strconv.Itoa(int(rand.Int31()))))
-		nearest_hash,nh_err := ch.Find(b64)
-		if nh_err != nil || nearest_hash == "" {
-			e := fmt.Sprintf("err returned on finding nearest hash for %s",b64)
+		nearestHash, nhErr := ch.Find(b64)
+		if nhErr != nil || nearestHash == "" {
+			e := fmt.Sprintf("err returned on finding nearest hash for %s", b64)
 			t.Errorf(e)
 		}
-		dist[nearest_hash]++
+		dist[nearestHash]++
 	}
-	for k,v := range dist {
-		fmt.Printf("%s %d (%f pct) \n",k,v,(float64(v)/float64(total))*100)
+	for k, v := range dist {
+		fmt.Printf("%s %d (%f pct) \n", k, v, (float64(v)/float64(total))*100)
 	}
 }
 
 func TestEmptyCH(t *testing.T) {
 	fmt.Printf("\n---\nTEST EMPTY\n")
-	ch := NewConsistentHash(0)
-	if ch != nil {
+	ch, chErr := NewConsistentHash(0)
+	if chErr == nil {
 		t.Errorf("should return nil when mult factor 0")
 	}
-	ch = NewConsistentHash(1)
-	if ch == nil {
+	ch, chErr = NewConsistentHash(1)
+	if chErr != nil {
 		t.Errorf("should return non-nil when mult factor nonzero")
 	}
-	_,err := ch.Find("hello")
+	_, err := ch.Find("hello")
 	if err == nil {
 		t.Errorf("should not find anything on empty hash")
 	}
@@ -89,13 +89,13 @@ func TestEmptyCH(t *testing.T) {
 
 func TestCollision(t *testing.T) {
 	fmt.Printf("\n---\nTEST COLLISION\n")
-	ch := NewConsistentHash(1)
-	insert_err := ch.Insert("hello")
-	if insert_err != nil {
-		t.Errorf(insert_err.Error())
+	ch, _ := NewConsistentHash(1)
+	insertErr := ch.Insert("hello")
+	if insertErr != nil {
+		t.Errorf(insertErr.Error())
 	}
-	insert_err  = ch.Insert("hello")
-	if insert_err == nil {
+	insertErr = ch.Insert("hello")
+	if insertErr == nil {
 		t.Errorf("should have caused collision")
 	}
 }
@@ -103,22 +103,22 @@ func TestCollision(t *testing.T) {
 func TestRemove(t *testing.T) {
 	fmt.Printf("\n---\nTEST REMOVE\n")
 	mult := 2
-	ch := NewConsistentHash(mult)
-	items := []string{"127.0.0.1","17.0.1.1","1.1.0.1","27.99.0.111","64.0.8.8","8.8.8.8","10.100.0.100",
-		"128.4.4.4","28.28.1.1","28.10.0.10","12.9.0.10","11.11.8.1","13.10.0.19","128.19.19.19"}
-	for _,item := range items {
-		insert_err := ch.Insert(item)
-		if insert_err != nil {
-			t.Errorf(insert_err.Error())
+	ch, _ := NewConsistentHash(mult)
+	items := []string{"127.0.0.1", "17.0.1.1", "1.1.0.1", "27.99.0.111", "64.0.8.8", "8.8.8.8", "10.100.0.100",
+		"128.4.4.4", "28.28.1.1", "28.10.0.10", "12.9.0.10", "11.11.8.1", "13.10.0.19", "128.19.19.19"}
+	for _, item := range items {
+		insertErr := ch.Insert(item)
+		if insertErr != nil {
+			t.Errorf(insertErr.Error())
 		}
 	}
-	
+
 	// remove something that isn't there
 	_ = ch.Remove("hello")
 
 	// make sure the len is correct
 	if len(ch.SumList) != (mult * len(items)) {
-		e := fmt.Sprintf("SumList len should be %d, but is %d",(mult*len(items)),len(ch.SumList))
+		e := fmt.Sprintf("SumList len should be %d, but is %d", (mult * len(items)), len(ch.SumList))
 		t.Errorf(e)
 	}
 
@@ -126,53 +126,53 @@ func TestRemove(t *testing.T) {
 	_ = ch.Remove("28.28.1.1")
 
 	// make sure the len is correct
-	if len(ch.SumList) != (mult * (len(items)-1)) {
-		e := fmt.Sprintf("SumList len should be %d, but is %d",(mult*len(items)),len(ch.SumList))
+	if len(ch.SumList) != (mult * (len(items) - 1)) {
+		e := fmt.Sprintf("SumList len should be %d, but is %d", (mult * len(items)), len(ch.SumList))
 		t.Errorf(e)
 	}
 
-	sum1 := crc32.ChecksumIEEE([]byte(mult_elt("28.28.1.1",1)))
-	sum2 := crc32.ChecksumIEEE([]byte(mult_elt("28.28.1.1",2)))
-	for _,v := range ch.SumList {
+	sum1 := crc32.ChecksumIEEE([]byte(multElt("28.28.1.1", 1)))
+	sum2 := crc32.ChecksumIEEE([]byte(multElt("28.28.1.1", 2)))
+	for _, v := range ch.SumList {
 		if v == sum1 || v == sum2 {
 			t.Errorf("found element in SumList that should have been deleted")
 		}
 	}
 
 	mult = 1
-	ch = NewConsistentHash(mult)
-	items = []string{"127.0.0.1","17.0.1.1","1.1.0.1","27.99.0.111","64.0.8.8","8.8.8.8","10.100.0.100",
-		"128.4.4.4","28.28.1.1","28.10.0.10","12.9.0.10","11.11.8.1","13.10.0.19","128.19.19.19"}
-	for _,item := range items {
-		insert_err := ch.Insert(item)
-		if insert_err != nil {
-			t.Errorf(insert_err.Error())
+	ch, _ = NewConsistentHash(mult)
+	items = []string{"127.0.0.1", "17.0.1.1", "1.1.0.1", "27.99.0.111", "64.0.8.8", "8.8.8.8", "10.100.0.100",
+		"128.4.4.4", "28.28.1.1", "28.10.0.10", "12.9.0.10", "11.11.8.1", "13.10.0.19", "128.19.19.19"}
+	for _, item := range items {
+		insertErr := ch.Insert(item)
+		if insertErr != nil {
+			t.Errorf(insertErr.Error())
 		}
 	}
 
 	// find the target for a source. then remove that target. then reinsert it and make sure
 	// source maps once to it once again
-	nearest,err := ch.Find("helloooo")
+	nearest, err := ch.Find("helloooo")
 	if err != nil {
 		t.Errorf(err.Error())
 	}
-	fmt.Printf("initially source maps to target %s\n",nearest)
+	fmt.Printf("initially source maps to target %s\n", nearest)
 	_ = ch.Remove(nearest)
-	nearest2,err2 := ch.Find("helloooo")
+	nearest2, err2 := ch.Find("helloooo")
 	if err2 != nil {
 		t.Errorf(err2.Error())
 	}
-	fmt.Printf("after removal of %s, source now maps to %s\n",nearest,nearest2)
-	insert_err := ch.Insert(nearest)
-	if insert_err != nil {
-		t.Errorf(insert_err.Error())
+	fmt.Printf("after removal of %s, source now maps to %s\n", nearest, nearest2)
+	insertErr := ch.Insert(nearest)
+	if insertErr != nil {
+		t.Errorf(insertErr.Error())
 	}
-	nearest3,err3 := ch.Find("helloooo")
+	nearest3, err3 := ch.Find("helloooo")
 	if err3 != nil {
 		t.Errorf(err3.Error())
 	}
-	fmt.Printf("after reinsertion of %s, source now maps to %s\n",nearest,nearest3)
-	if (nearest != nearest3) {
+	fmt.Printf("after reinsertion of %s, source now maps to %s\n", nearest, nearest3)
+	if nearest != nearest3 {
 		t.Errorf("was not able to round trip removal and addition of target")
 	}
 }
